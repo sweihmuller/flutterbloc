409c652dd8a3a89651fa316ed36bb864b570c254
Santi <sweihmuller01@gmail.com>
2025-09-29 16:37:17 -0300

    BLoC Provider & Multi BLoC Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
+
 import 'package:blocs_app/config/config.dart';
 
-void main() => runApp(const MyApp());
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:blocs_app/presentation/blocs/01-simple_cubit/username_cubit.dart';
+
+void main() {
+  runApp(const BlocsProviders());
+}
+
+class BlocsProviders extends StatelessWidget {
+  const BlocsProviders({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [BlocProvider(create: (context) => UsernameCubit())],
+        child: MyApp());
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp.router(
       title: 'Flutter BLoC',
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
-      theme: AppTheme( isDarkmode: false ).getTheme(),
+      theme: AppTheme(isDarkmode: false).getTheme(),
     );
   }
 }
